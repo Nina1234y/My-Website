588bf4bc7ebc1b9650246c47af7614e9848aaefd
nina1234y <nina27@my.yorku.ca>
2020-10-10 22:01:25 -0400

    update new website

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("home").onclick = function () {
+    location.href = "index.html";
+};
+
+document.getElementById("about").onclick = function () {
+    location.href = "about.html";
+};
+
+document.getElementById("work").onclick = function () {
+    location.href = "work.html";
+};
+
+document.getElementById("projects").onclick = function () {
+    location.href = "projects.html";
+};
+
+document.getElementById("contact").onclick = function () {
+    location.href = "contact.html";
+};
